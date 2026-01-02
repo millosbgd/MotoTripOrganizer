7bd2cf1b7fa3d93634b9e54142c7eae68a95ce3b
Milos Novakovic <milos.novakovic@cycle.rs>
2026-01-02 10:25:53 +0100

    Fix Next.js standalone deployment for Azure App Service

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: ['localhost', 'mototriporgdevst.blob.core.windows.net'],
   },
